9823c884ff45a22b0bd7e9f916466d72a6908ba0
Sayed <muhammadsaye741@gmail.com>
2026-05-12 20:46:39 +0600

    Add about section

--- src/Component/AboutPlatform.jsx
+++ src/Component/AboutPlatform.jsx
@@ -3,7 +3,25 @@
 const AboutPlatform = () => {
   return (
     <div>
-      
+      <h1 className="font-extrabold text-amber-500 text-3xl text-center mt-9 underline ">
+        About Platform
+      </h1>
+
+      <div className="border-3 p-3 rounded-2xl border-amber-500 mt-3 ">
+        <div>
+          <h1 className="font-bold text-2xl text-center">Your Ultimate</h1>
+          <h1 className="font-bold text-2xl text-amber-500 text-center">Movie Universe</h1>
+
+          <p className="text-gray-400 my-3 text-center">MovieMaster Pro is the most comprehensive movie management system ever built. Discover, curate, and organize your Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolorem ex dolore esse error, illo molestiae quia placeat omnis obcaecati! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sunt repellendus ipsum architecto impedit, beatae dignissimos quam cum vero velit enim, et, accusantium quia porro possimus saepe! Expedita, obcaecati quas. personal film library with powerful tools designed for true cinema enthusiasts.</p>
+
+          <p className="text-gray-500"><span className="font-semibold text-black">Personal Collections</span> — Add and manage your own movies with full CRUD control</p>
+          <p className="text-gray-500 my-1"><span className="font-semibold text-black">Smart Filtering</span> —  Filter by genre, rating range, year, and more</p>
+          <p className="text-gray-500"><span className="font-semibold text-black">Watchlist </span> — Save movies to watch later with one click</p>
+          <p className="text-gray-500 my-1"><span className="font-semibold text-black">Dark & Light Themes </span> — Comfortable viewing in any environment</p>
+          <p className="text-gray-500"><span className="font-semibold text-black">Responsive Design </span> — Perfect experience on all devices</p>
+        </div>
+
+      </div>
     </div>
   );
 };
