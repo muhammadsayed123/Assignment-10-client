6bb6ebcf12dfde65fb10b6892bd882523741be56
Sayed <muhammadsaye741@gmail.com>
2026-05-13 20:01:36 +0600

    Solve firebase system

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPq6gdz92VMH2A0kM36ofBcuER82GuXR4",
-  authDomain: "movie-master-a95e1.firebaseapp.com",
-  projectId: "movie-master-a95e1",
-  storageBucket: "movie-master-a95e1.firebasestorage.app",
-  messagingSenderId: "510934735714",
-  appId: "1:510934735714:web:6508221440d74afbb8ea3b",
-  measurementId: "G-G3CVV746C3"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
